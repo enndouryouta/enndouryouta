55b8f76e69bbfd06c319ecb315637ddbfcd7da82
enndouryouta <131093438+enndouryouta@users.noreply.github.com>
2023-11-07 10:12:54 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,2 +1,1 @@
-function gazo688()
-{document.getElementById('who688').innerHTML='<img src="./image/爆発2.jpg" alt="爆発2">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/爆発2.jpg" alt="爆発2">';}

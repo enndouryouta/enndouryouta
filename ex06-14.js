7b5b8b95b529c073e3913e9b66c9d1b554336586
enndouryouta <131093438+enndouryouta@users.noreply.github.com>
2023-11-21 10:32:19 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,2 +1,2 @@
-function gazo1(){document.getElementById('logo1').src='./image/魔理沙.jpg';}
-function red(){document.getElementById('logo1').src='./image/霊夢.jpg';}
+function gazo1(){document.getElementById('logo1').src='./image/熱狂4人組.jpg';}
+function red(){document.getElementById('logo1').src='./image/海外しんみり.jpg';}

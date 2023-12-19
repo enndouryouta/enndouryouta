334d030545d12b6534402202c7f0a570b98a41e9
enndouryouta <131093438+enndouryouta@users.noreply.github.com>
2023-12-19 10:22:47 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,5 +1,5 @@
 function hels(){
 var x=document.getElementById('体重1').value;
 var y=document.getElementById('身長1').value;
-alert(Number(x)/(Number(y)*Number(y)));    
+alert(Number(x)/((Number(y)/100)*(Number(y)/100)));    
 }  
